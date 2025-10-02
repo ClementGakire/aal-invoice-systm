f858ed958bddccd2845814ecf1ccc269718ad6e8
Clement Gakire <clementgakire@gmail.com>
2025-10-02 16:55:46 +0200

    scaling printable invoice

--- src/components/PrintableInvoice.tsx
+++ src/components/PrintableInvoice.tsx
@@ -483,7 +483,7 @@ export default function PrintableInvoice({
             {/* Header */}
             <div className="flex items-start justify-between mb-3 print:mb-1">
               <div className="flex items-center gap-3 print:gap-1">
-                <div className="w-32 h-32 print:w-16 print:h-16">
+                <div className="w-32 h-32 print:w-32 print:h-32">
                   <img
                     src={companyConfig.logo}
                     alt={`${companyConfig.name} Logo`}
